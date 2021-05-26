86bc6aa3d618869ecb3991aacbf78bc567ebd7d3
ghidyon <gideonakunana@gmail.com>
2021-05-26 15:09:09 +0100

    added login route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,6 +4,10 @@ const router = express.Router();
 // Import book controllers
 const AuthController = require('../controllers/authControllers');
 
+// Register a new user
 router.post('/signup', AuthController.registerNewUser);
+
+// Login a user
+router.post('/login', AuthController.loginUser);
 
 module.exports = router;
