57ace917dd187dfc4879b4c817b1c80bf2ac3fba
ghidyon <gideonakunana@gmail.com>
2021-05-29 11:52:21 +0100

    added authentication for checking admin

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,3 +1,9 @@
+/* 
+    TODO
+    - Authenticate user to have a token that will be used to allow requests on all book routes
+    - Therefore, they must be logged in before they can do anything on the application
+*/
+
 const jwt = require('jsonwebtoken');
 const privateKey = '~no_none';
 
@@ -12,12 +18,23 @@ exports.authenticateUser = (req, res, next) => {
     }
     // decode token
     let token = splittedHeader[1];
+    // If token is valid, returns user's decoded payload
     jwt.verify(token, privateKey, (err, decodedToken) => {
         if (err) return res.status(500).json({ err });
         // check if it's valid
-        if (!decodedToken) return res.status(401).json({ message: "invalid authorization token, please login" })
-        
+        if (!decodedToken) return res.status(401).json({ message: "invalid authorization token, please login" });
+
+        // Add user payload to request, so as to make user accessible outside this function
+        req.user = decodedToken;
         // allow user to continue with the request
         next(); // when the conditions in this function is fulfilled, next() calls the next function placed after it.
     });
 }
+
+exports.checkIfAdmin = (req, res, next) => {
+    let { role } = req.user;
+    if (role !== 'admin') {
+        return res.status(401).json({ message: 'this route is restricted to admin users' });
+    }
+    return next();
+}  
