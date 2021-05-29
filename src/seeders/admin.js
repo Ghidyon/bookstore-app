1a962f6a73bccacfc395839233debf0c6c844625
ghidyon <gideonakunana@gmail.com>
2021-05-29 08:42:05 +0100

    added admin seeder

---
+++ src/seeders/admin.js
@@ -0,0 +1,34 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+let passCode = 'support';
+
+exports.seedAdmin = () => {
+    // Check if there's an admin account
+    User.findOne({ role: 'admin' }, (err, admin) => {
+        if (err) throw err;
+        if (admin) return "admin already exists!";
+        
+        // Then create an admin account
+        User.create({
+            firstName: 'Book',
+            lastName: 'Goblin',
+            username: 'bookgoblin',
+            role: 'admin'
+        }, (err, user) => {
+            if (err) throw err;
+            // hash the password
+            bcrypt.genSalt(10, (err, salt) => {
+                if (err) throw err;
+                bcrypt.hash(passCode, salt, (err, hash) => {
+                    if (err) throw err;
+                    user.password = hash;
+                    user.save((err, savedUser) => {
+                        if (err) throw err;
+                        return "admin account created!";
+                    });
+                });
+            });
+        });
+
+    })
+}
